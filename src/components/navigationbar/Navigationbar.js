da1eedcb8dbd0b23af8a3bd8fbe38d000814287b
KostyaWest <illipurov2134@gmail.com>
2024-11-20 23:23:54 +0400

    Ссылки заработали

--- src/components/navigationbar/Navigationbar.js
+++ src/components/navigationbar/Navigationbar.js
@@ -11,12 +11,21 @@ import moon from "./../../img/icons/moon.svg"
 import "./style.css";
 //для импортов добавили выход два раза вверх по сравнению с app.js
 
+//делаем ссылки из реакт роутердом
+import { NavLink } from "react-router-dom";
+
 const Navigationbar = () => {
     return ( 
     <nav className="nav">
         <div className="container">
             <div className="nav-row">
-                <a href="./index.html" className="logo"><strong>Freelancer</strong> portfolio</a>
+                <NavLink to="/"
+                  className="logo">
+
+                    <strong>Freelancer</strong> portfolio
+
+                </NavLink> 
+                {/* первая ссылка. Когда наводим на фрилансер портфолио - выкидывает на локалхост 3000 */}
 
                 <button className="dark-mode-btn">
                     <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
@@ -24,9 +33,24 @@ const Navigationbar = () => {
                 </button>
 
                 <ul className="nav-list">
-                    <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                    <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                    <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
+                    <li className="nav-list__item">
+                        <NavLink to = "/"
+                          className = "nav-list__link"> 
+                          Home 
+                        </NavLink>
+                    </li>
+                    <li className="nav-list__item">
+                        <NavLink to = "/projects"
+                          className = "nav-list__link">
+                          Projects
+                        </NavLink>      
+                    </li>
+                    <li className="nav-list__item">
+                        <NavLink to = "/contacts"
+                          className = "nav-list__link">
+                          Contacts
+                        </NavLink>
+                    </li>
                 </ul>
             </div>
         </div>
