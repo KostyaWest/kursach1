413b3ddb6e2125cd63c3912d0c45e69d71bf3f17
KostyaWest <illipurov2134@gmail.com>
2024-11-20 23:37:16 +0400

    Не получились подчеркивания ссылки. в будущем исправить. сейчас буду исправлять скроллбар

--- src/components/navigationbar/Navigationbar.js
+++ src/components/navigationbar/Navigationbar.js
@@ -15,6 +15,11 @@ import "./style.css";
 import { NavLink } from "react-router-dom";
 
 const Navigationbar = () => {
+
+
+    const activeLink = "nav-list__link nav-list_link-active"; //должно быть подчеркивание (выделение для ссылки, но оно не работает)
+    const normalLink = "nav-list__link";
+
     return ( 
     <nav className="nav">
         <div className="container">
@@ -35,19 +40,19 @@ const Navigationbar = () => {
                 <ul className="nav-list">
                     <li className="nav-list__item">
                         <NavLink to = "/"
-                          className = "nav-list__link"> 
+                          className = {({isActive}) =>  isActive ? activeLink : normalLink}> 
                           Home 
                         </NavLink>
                     </li>
                     <li className="nav-list__item">
                         <NavLink to = "/projects"
-                          className = "nav-list__link">
+                          className = {({isActive}) =>  isActive ? activeLink : normalLink}>
                           Projects
                         </NavLink>      
                     </li>
                     <li className="nav-list__item">
                         <NavLink to = "/contacts"
-                          className = "nav-list__link">
+                          className = {({isActive}) =>  isActive ? activeLink : normalLink}>
                           Contacts
                         </NavLink>
                     </li>
